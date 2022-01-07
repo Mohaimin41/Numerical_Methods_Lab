c0402196c12a3978ab2b95579d7e62cd7c1e1d3d
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2022-01-07 17:56:11 +0600

    gaussian works-ish

--- offline2_Gaussian/1905041.py
+++ offline2_Gaussian/1905041.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-# we set printOption for numpy globally as 4 digits after decimal
-# np.set_printoptions(formatter={'float': '{: 0.4f}'.format})
-# np.set_printoptions(precision=4)
-
 def pivotForwardElimination(mat_a, mat_b, pivotRow, numOfVar):
 
     largestRow = pivotRow
@@ -42,11 +38,18 @@ def BackSubstitution(mat_a, mat_b, mat_res, numOfVar):
 
 def ForwardElimination(mat_a, mat_b, numOfVar, pivot=True, showall=True):
     for row in range(numOfVar - 1):
+        #track variable for printing information
+        subStep = 0
+        
+        if (showall):
+            print("Step: ", row + 1)
 
         if (pivot):
             mat_a, mat_b = pivotForwardElimination(mat_a, mat_b, row, numOfVar)
 
         for nextRow in range(row + 1, numOfVar):
+            subStep += 1
+            
             multipler = mat_a[nextRow][row] / mat_a[row][row]
 
             currA_row = mat_a[row] * multipler
@@ -55,10 +58,14 @@ def ForwardElimination(mat_a, mat_b, numOfVar, pivot=True, showall=True):
             mat_a[nextRow] -= currA_row
             mat_b[nextRow] -= currB_row
 
-        if (showall):
-            with np.printoptions(precision=4, suppress=True):
-                print(mat_a)
-                print(mat_b)
+            if (showall):
+                print("Sub Step: " , subStep )
+                with np.printoptions(precision=4, suppress=True):
+                    print("Co-efficient Matrix")
+                    print(mat_a)
+                    print("Constant matrix")
+                    print(mat_b)
+                print("\n")
 
     return mat_a, mat_b
 
@@ -67,8 +74,6 @@ def GaussianElimination(mat_a, mat_b, pivot=True, showAll=True):
 
     mat_a = np.array(mat_a, dtype=np.double)
     mat_b = np.array(mat_b, dtype=np.double)
-
-    #mat_res = np.array(dtype=np.double )
 
     numOfVar, numOfCol = mat_a.shape
     mat_res = np.zeros((numOfVar, 1))
@@ -89,17 +94,6 @@ def inputMatrix(rows, cols):
     
     return mat_res
 
-
-# mat_a = np.array([[25, 5, 1], [64, 8, 1], [144, 12, 1]])
-# # mat_a = np.array([[10, -7, 0], [-3, 2.099, 6], [5, -1, 5]])
-# mat_a = np.array([[20, 15, 10], [-3, -2.249, 7], [5, 1, 3]])
-
-# mat_b = np.array([[106.8], [177.2], [279.2]])
-# # mat_b = np.array([[7], [3.901], [6]])
-# mat_b = np.array([[45], [1.751], [9]])
-
-# print(mat_a, mat_b)
-# print(mat_a.shape, mat_b.shape)
 variables = int(input())
 
 mat_a = inputMatrix(variables, variables)
