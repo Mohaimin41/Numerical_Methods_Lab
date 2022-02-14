566f79ac54d85c62d02b82cb32160ac3491dcdf3
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2022-02-14 20:01:35 +0600

    works ish

--- online_regression/test.py
+++ online_regression/test.py
@@ -1,4 +1,3 @@
-from turtle import color
 import matplotlib.pyplot as plt
 import numpy as np
 import regression as reg
@@ -7,8 +6,22 @@ import regression as reg
 # linear regression example
 angle = [0.698132, 0.959931, 1.134464, 1.570796, 1.919862]
 torque = [0.188224, 0.209138, 0.230052, 0.250965, 0.313707]
-# (k_1, k_2) = reg.linear_reg(angle, torque)
-# print(k_1, k_2)
+(k_1, k_2) = reg.linear_reg(angle, torque)
+print(k_1, k_2)
+
+plt.title("Experimental Datas vs Solved Curve")
+plt.xlabel("angle")
+plt.ylabel("torque")
+
+plt.scatter(np.array(angle), np.array(torque), color="green")
+# plt.plot(np.array(time), np.array(radiation), color="green")
+
+plt.plot(np.array(angle), k_1 + k_2 * np.array(angle), color="red")
+
+plt.show()
+
+
+
 
 # exponential regression example
 time = [0, 1, 3, 5, 7, 9]
@@ -37,17 +50,17 @@ temp = [80, 40, -40, -120, -200, -280, -340]
 coeff_thermal_expansion = np.array(
     [6.47, 6.24, 5.72, 5.09, 4.30, 3.33, 2.45]) * 1e-6
 result = reg.polynomial_reg(temp, coeff_thermal_expansion, 2)
-print(result)
+# print(result)
 
-plt.title("Experimental Datas vs Solved Curve")
-plt.xlabel("temp")
-plt.ylabel("thermal expansion coefficient")
+# plt.title("Experimental Datas vs Solved Curve")
+# plt.xlabel("temp")
+# plt.ylabel("thermal expansion coefficient")
 
-plt.scatter(np.array(temp), np.array(coeff_thermal_expansion), color="green")
-# plt.plot(np.array(time), np.array(radiation), color="green")
+# plt.scatter(np.array(temp), np.array(coeff_thermal_expansion), color="green")
+# # plt.plot(np.array(time), np.array(radiation), color="green")
 
-plt.plot(np.array(temp), result[0][0] + result[1][0] * np.array(temp) 
-                        + result[2][0] * np.array(temp) * np.array(temp),
-                         color="red")
+# plt.plot(np.array(temp), result[0][0] + result[1][0] * np.array(temp) 
+#                         + result[2][0] * np.array(temp) * np.array(temp),
+#                          color="red")
 
-plt.show()
+# plt.show()
